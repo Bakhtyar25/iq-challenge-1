3d87d23159b3ca590ad095050b79ac83c5483649
bakhtyar25 <bakhtyarmhamad12@gmail.com>
2024-08-06 22:40:51 +0300

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between px-6  relative">
-      <main className="w-full pt-24">
+      <main className="w-full pt-16 xs:pt-44 md:pt-24">
         <div className="flex flex-col items-center gap-14 md:gap-24">
           <h1 className="text-center text-lg font-bold uppercase tracking-[0.3em] text-white md:text-xl">
             We&apos;re launching soon
